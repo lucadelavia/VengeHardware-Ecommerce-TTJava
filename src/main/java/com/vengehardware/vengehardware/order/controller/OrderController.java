26bb2b3162a8823c4121ee481f33cb69f0ab2223
luca <lucadelavia@gmail.com>
2025-07-23 10:30:22 -0300

    cambios: se agrega Front funcional

--- src/main/java/com/vengehardware/vengehardware/order/controller/OrderController.java
+++ src/main/java/com/vengehardware/vengehardware/order/controller/OrderController.java
@@ -1,9 +1,12 @@
 package com.vengehardware.vengehardware.order.controller;
 
 import com.vengehardware.vengehardware.order.model.Order;
+import com.vengehardware.vengehardware.order.repository.OrderRepository;
 import com.vengehardware.vengehardware.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -12,9 +15,15 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderRepository orderRepository;
 
     @PostMapping
     public Order createOrder(@RequestBody Order order) {
         return orderService.createOrder(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
     }
 }
--- src/main/java/com/vengehardware/vengehardware/order/model/Order.java
+++ src/main/java/com/vengehardware/vengehardware/order/model/Order.java
@@ -1,9 +1,16 @@
 package com.vengehardware.vengehardware.order.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "orders")
@@ -11,18 +18,20 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Builder.Default
     private LocalDateTime date = LocalDateTime.now();
 
     private int productId;
     private int quantity;
     private double unitPrice;
     private double total;
+
+    @Builder.Default
     private String status = "pending";
 }
